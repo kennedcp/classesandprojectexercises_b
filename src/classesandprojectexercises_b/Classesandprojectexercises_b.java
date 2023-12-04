7616104bbefc31e8f582f9cacd8e3fb1f5e8f3e4
kennedcp <130641094+kennedcp@users.noreply.github.com>
2023-12-04 19:14:17 +0000

    HelloWorld!

--- src/classesandprojectexercises_b/Classesandprojectexercises_b.java
+++ src/classesandprojectexercises_b/Classesandprojectexercises_b.java
@@ -15,6 +15,7 @@ public class Classesandprojectexercises_b {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("HelloWorld!");
     }
     
 }
